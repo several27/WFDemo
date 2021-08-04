6ea6e70409081f591f406a2b1e80214e1f4b802f
Maciej <demo5@simpledatalabs.com>
2021-08-04 23:08:28 +0000

    Deleting content

--- workflows/DemoWorkflow/code/src/main/scala/config/Config.scala
+++
@@ -1,9 +0,0 @@
-package config
-
-import io.prophecy.libs._
-import pureconfig.ConfigReader.Result
-import pureconfig._
-import pureconfig.generic.ProductHint
-import pureconfig.generic.auto._
-
-case class Config(fabricName: String, @Description("") column_name: String) extends ConfigBase
--- workflows/DemoWorkflow/code/src/main/scala/udfs/UDFs.scala
+++
@@ -1,5 +0,0 @@
-package udfs
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.SparkSession
-object UDFs { def registerUDFs(spark: SparkSession) = {} }
